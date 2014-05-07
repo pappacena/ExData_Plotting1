5c3650dbc6d1d4195c089edb8d090136da541097
Thiago F Pappacena <pappacena@gmail.com>
2014-05-06 21:34:09 -0300

    adding plot3 first tests

---
+++ plot3.R
@@ -0,0 +1,20 @@
+lines <- grep("^[1-2]/2/2007", readLines("household_power_consumption.txt"), value=TRUE)
+values <- read.csv(textConnection(lines), sep=";")
+names(values) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+values$Date <- as.Date(values$Date, format="%d/%m/%Y")
+values$DateTime <- strptime(paste(values$Date, values$Time, sep=" "), format="%Y-%m-%d %H:%M:%S")
+
+values <- subset(values, !is.na(Global_active_power))
+
+png("plot3.png", width=480, height=480, units="px", bg="transparent")
+with(values, {
+    plot(DateTime, Global_active_power, type="n",
+         xlab="", ylab="Energy sub metering")
+    lines(DateTime, Sub_metering_1, col="black")
+    lines(DateTime, Sub_metering_2, col="red")
+    lines(DateTime, Sub_metering_3, col="blue")
+    legend("topright", lwd=1, 
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+           col=c("black", "red", "blue"))
+})
+dev.off()
